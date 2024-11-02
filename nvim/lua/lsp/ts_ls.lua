cdeed3d58231b3605571d64c90b14e9e29b058e0
Ben Lovell <ben.j.lovell@gmail.com>
2024-11-02 14:36:11 +0100

    Update from tsserver to ts_ls (new name)

--- nvim/lua/lsp/ts_ls.lua
+++ nvim/lua/lsp/ts_ls.lua
@@ -1,11 +1,11 @@
--- [nfnl] Compiled from fnl/lsp/tsserver.fnl by https://github.com/Olical/nfnl, do not edit.
+-- [nfnl] Compiled from fnl/lsp/ts_ls.fnl by https://github.com/Olical/nfnl, do not edit.
 local _local_1_ = require("nfnl.module")
 local autoload = _local_1_["autoload"]
 local _local_2_ = autoload("lspconfig")
-local tsserver = _local_2_["tsserver"]
+local ts_ls = _local_2_["ts_ls"]
 local function setup(opts)
   if (1 == vim.fn.executable("typescript-language-server")) then
-    return tsserver.setup(opts)
+    return ts_ls.setup(opts)
   else
     return nil
   end
